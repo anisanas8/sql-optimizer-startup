1c00632dcd1cc863ec7bd0aa010c8d95533570af
Anas <anissandur8@gmail.com>
2026-07-08 12:20:56 +0530

    added google verification

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ def read_root():
     <!DOCTYPE html>
     <html lang="en">
     <head>
+        <head>
         <meta charset="UTF-8">
+        <meta name="google-site-verification" content="PASTE_YOUR_COPIED_GOOGLE_CODE_HERE" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SQL Performance Optimizer</title>
         <style>
